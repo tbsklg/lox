d209bba04805354c1a55ff6b70e8922ae37930be
Tobias Klug <tbs.klg@gmail.com>
2024-10-13 14:00:50 +0200

    Uppercase please

--- src/lib.rs
+++ src/lib.rs
@@ -12,8 +12,8 @@ pub enum TokenType {
 impl fmt::Display for TokenType {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            TokenType::LeftParen => write!(f, "{}", "Left_Paren"),
-            TokenType::RightParen => write!(f, "{}", "Right_Paren"),
+            TokenType::LeftParen => write!(f, "{}", "LEFT_PAREN"),
+            TokenType::RightParen => write!(f, "{}", "RIGHT_PAREN"),
             TokenType::EOF => write!(f, "{}", "EOF"),
         }
     }
