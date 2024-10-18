8b99966cc73772ea29b86f7686b76ad21a984501
Tobias Klug <tbs.klg@gmail.com>
2024-10-18 08:03:48 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This repository contains my solution of (Build your own interpreter challenge)[https://app.codecrafters.io/courses/interpreter/stages/sc2].
---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use std::env;
+use std::fs;
+
+use codecrafters_interpreter::*;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 3 {
+        eprintln!("Usage: {} tokenize <filename>", args[0]);
+        return;
+    }
+
+    let command = &args[1];
+    let filename = &args[2];
+    let mut exit_code = 0;
+
+    match command.as_str() {
+        "tokenize" => {
+            let file_contents = fs::read_to_string(filename).unwrap_or_else(|_| {
+                eprintln!("Failed to read file {}", filename);
+                String::new()
+            });
+
+            for token in Lexer::new(file_contents.chars()) {
+                match token {
+                    Ok(token) => println!("{}", token),
+                    Err(e) => {
+                        eprintln!("{}", e);
+                        exit_code = 65;
+                    }
+                }
+            }
+            println!("EOF  null"); // Placeholder, remove this line when implementing the scanner
+        }
+        _ => {
+            eprintln!("Unknown command: {}", command);
+            return;
+        }
+    }
+
+    std::process::exit(exit_code)
+}
